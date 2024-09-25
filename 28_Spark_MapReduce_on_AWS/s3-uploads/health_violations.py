3bd3705ef6c7f1f59f385692bafde0762fd0e894
Andre Jacobs <andrejacobs365@gmail.com>
2024-09-25 08:31:35 -0700

    updating files

--- 28_Spark_MapReduce_on_AWS/s3-uploads/health_violations.py
+++ 28_Spark_MapReduce_on_AWS/s3-uploads/health_violations.py
@@ -19,11 +19,13 @@ def calculate_red_violations(data_source, output_uri):
         restaurants_df.createOrReplaceTempView("restaurant_violations")
 
         # Create a DataFrame of the top 10 restaurants with the most Red violations
-        top_red_violation_restaurants = spark.sql("""SELECT name, count(*) AS total_red_violations 
-          FROM restaurant_violations 
-          WHERE violation_type = 'RED' 
-          GROUP BY name 
-          ORDER BY total_red_violations DESC LIMIT 10""")
+        top_red_violation_restaurants = spark.sql("""
+            SELECT name, count(*) AS total_red_violations 
+            FROM restaurant_violations 
+            WHERE violation_type = 'RED' 
+            GROUP BY name 
+            ORDER BY total_red_violations DESC LIMIT 10
+        """)
 
         # Write the results to the specified output URI
         top_red_violation_restaurants.write.option("header", "true").mode("overwrite").csv(output_uri)
